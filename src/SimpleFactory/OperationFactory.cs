95b19cb990e8a398bdd788f84693a68725f6a607
Katrina Leng <lengkat@qq.com>
2020-06-30 15:41:17 +0800

    initialization

---
+++ src/SimpleFactory/OperationFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SimpleFactory
+{
+    public class OperationFactory
+    {
+        public static Operation CreatOperate(string operate)
+        {
+            Operation oper = null;
+            switch (operate)
+            {
+                case "+":
+                    oper = new OperationAdd();
+                    break;
+                case "-":
+                    oper = new OperationSub();
+                    break;
+                case "*":
+                    oper = new OperationMul();
+                    break;
+                case "/":
+                    oper = new OperationDiv();
+                    break;
+                default:
+                    throw new Exception("input error");
+            }
+
+            return oper;
+        }
+
+    }
+}
