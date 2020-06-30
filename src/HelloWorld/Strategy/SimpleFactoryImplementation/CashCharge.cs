5e729f01ef85b673a4ce47560936f205345aa376
Katrina Leng <lengkat@qq.com>
2020-06-30 15:48:29 +0800

    Add Strategy

---
+++ src/HelloWorld/Strategy/SimpleFactoryImplementation/CashCharge.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Strategy.SimpleFactoryImplementation
+{
+    abstract class CashCharge
+    {
+        public abstract double GetResult(double money);
+    }
+
+    class CashNormal : CashCharge
+    {
+        public override double GetResult(double money)
+        {
+            return money;
+        }
+    }
+
+    class CashRebate : CashCharge
+    {
+        private readonly double _moneyRebate;
+        public CashRebate(string moneyRebate)
+        {
+            _moneyRebate = double.Parse(moneyRebate);
+        }
+
+        public override double GetResult(double money)
+        {
+            return money * _moneyRebate;
+        }
+    }
+
+    class CashReturn : CashCharge
+    {
+        private readonly double _moneyCondition;
+        private readonly double _moneyReturn;
+        public CashReturn(string moneyCondition, string moneyReturn)
+        {
+            _moneyCondition = double.Parse(moneyCondition);
+            _moneyReturn = double.Parse(moneyReturn);
+        }
+
+        public override double GetResult(double money)
+        {
+            var result = money;
+            if (money > _moneyCondition)
+            {
+                result = money - Math.Floor(money / _moneyCondition) * _moneyReturn;
+            }
+
+            return result;
+        }
+    }
+}
