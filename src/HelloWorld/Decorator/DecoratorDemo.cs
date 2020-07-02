c342e57efa18abfdb95d40cb6d7e03ba81749cd6
Katrina Leng <lengkat@qq.com>
2020-07-02 18:30:44 +0800

    Decorator

---
+++ src/HelloWorld/Decorator/DecoratorDemo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class DecoratorDemo
+    {
+        static void Main(string[] args)
+        {
+            //NewMethod();
+            NewMethod1();
+        }
+
+        private static void NewMethod1()
+        {
+            Person pr = new Person("Leng");
+            Console.WriteLine("first look");
+
+            Finery tShirts = new TShirts();
+            tShirts.Show();
+            Finery bigTrouser = new BigTrouser();
+            bigTrouser.Show();
+            Finery sneakers = new Sneakers();
+            sneakers.Show();
+            pr.Show();
+
+            Console.WriteLine("\nsecond look");
+            Finery suit = new Suit();
+            suit.Show();
+            Finery tie = new Tie();
+            tie.Show();
+            Finery leatherShoes = new LeatherShoes();
+            leatherShoes.Show();
+            pr.Show();
+
+            Console.Read();
+        }
+
+        private static void NewMethod()
+        {
+            Person ps = new Person("Leng");
+            Console.WriteLine("first look");
+            ps.WearShirts();
+            ps.WearBigTrouser();
+            ps.WearSneakers();
+            ps.Show();
+
+            Console.WriteLine("\nsecond look");
+            ps.WearSuit();
+            ps.WearTie();
+            ps.WearLeatherShoes();
+            ps.Show();
+
+            Console.Read();
+        }
+    }
+}
---
+++ src/HelloWorld/Decorator/Finery.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Decorator
+{
+    abstract class Finery
+    {
+        public abstract void Show();
+    }
+
+    class TShirts : Finery
+    {
+        public override void Show()
+        {
+            Console.WriteLine("T-shirt");
+        }
+    }
+
+    class BigTrouser : Finery
+    {
+        public override void Show()
+        {
+            Console.WriteLine("BigTrouser");
+        }
+    }
+
+    class Sneakers : Finery
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Sneakers");
+        }
+    }
+
+    class Suit : Finery
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Suit");
+        }
+    }
+
+    class Tie : Finery
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Tie");
+        }
+    }
+
+    class LeatherShoes : Finery
+    {
+        public override void Show()
+        {
+            Console.WriteLine("LeatherShoes");
+        }
+    }
+}
