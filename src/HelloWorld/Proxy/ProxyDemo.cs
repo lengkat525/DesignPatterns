0c2040f8aef9b3fca0c9c9c40da88859814cb9e4
Katrina Leng <lengkat@qq.com>
2020-07-06 16:20:29 +0800

    ProxyDemo

--- src/HelloWorld/Proxy/ProxyDemo.cs
+++ src/HelloWorld/Proxy/ProxyDemo.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Proxy
 {
-    class Program
+    class ProxyDemo
     {
         static void Main(string[] args)
         {
+            // NewMethod();
+            //NewMethod1();
+            Girl girl = new Girl {Name = "leng"};
+            Proxy proxy = new Proxy(girl);
+            proxy.GiveDolls();
+            proxy.GiveFlowers();
+            proxy.GiveChocolate();
+            Console.Read();
+        }
+
+        private static void NewMethod1()
+        {
+            Girl leng = new Girl {Name = "kat"};
+
+            Proxy yi = new Proxy(leng);
+            yi.GiveChocolate();
+            yi.GiveDolls();
+            yi.GiveFlowers();
+
+            Console.Read();
+        }
+
+        private static void NewMethod()
+        {
+            Girl leng = new Girl {Name = "kat"};
+
+            Pursuit yi = new Pursuit(leng);
+            yi.GiveChocolate();
+            yi.GiveDolls();
+            yi.GiveFlowers();
+
+            Console.Read();
         }
     }
 }
