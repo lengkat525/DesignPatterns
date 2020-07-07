f4d1caba534ed2d061dbe0d92c76de8a00b48b92
Katrina Leng <lengkat@qq.com>
2020-07-07 17:21:09 +0800

    Implementation of Leifeng with FactoryMethod

--- src/HelloWorld/FactoryMethod/FactoryMethodDemo.cs
+++ src/HelloWorld/FactoryMethod/FactoryMethodDemo.cs
@@ -8,6 +8,19 @@ namespace FactoryMethod
         {
             //NewMethod();
             //NewMethod1();
+            //NewMethod2();
+            IFactory factory = new UndergraduateFactory();
+            IFactory factory2 = new VolunteerFactory();
+            LeiFeng stu = factory.CreateLeiFeng();
+            LeiFeng vol = factory2.CreateLeiFeng();
+           
+            stu.Sweep();
+            vol.BuyRice();
+            Console.ReadLine();
+        }
+
+        private static void NewMethod2()
+        {
             LeiFeng stuA = SimpleFactory.CreateLeiFeng("Undergraduate");
             stuA.Sweep();
             LeiFeng volA = SimpleFactory.CreateLeiFeng("Volunteer");
--- src/HelloWorld/FactoryMethod/IFactory.cs
+++ src/HelloWorld/FactoryMethod/IFactory.cs
@@ -1,8 +1,11 @@
-﻿namespace FactoryMethod
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FactoryMethod
 {
     interface IFactory
     {
         Operation CreateOperation();
+        LeiFeng CreateLeiFeng();
     }
 
     class  AddFactory: IFactory
@@ -10,6 +13,11 @@
         public Operation CreateOperation()
         {
             return new OperationAdd();
+        }
+
+        public LeiFeng CreateLeiFeng()
+        {
+            throw new System.NotImplementedException();
         }
     }
 
@@ -19,6 +27,10 @@
         {
             return new OperationSub();
         }
+        public LeiFeng CreateLeiFeng()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 
     class MulFactory : IFactory
@@ -26,6 +38,10 @@
         public Operation CreateOperation()
         {
             return  new OperationMul();
+        }
+        public LeiFeng CreateLeiFeng()
+        {
+            throw new System.NotImplementedException();
         }
     }
 
@@ -35,5 +51,36 @@
         {
             return new OperationDiv();
         }
+        public LeiFeng CreateLeiFeng()
+        {
+            throw new System.NotImplementedException();
+        }
     }
+
+    class  UndergraduateFactory: IFactory
+    {
+        public Operation CreateOperation()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public LeiFeng CreateLeiFeng()
+        {
+            return  new Undergraduate();
+        }
+    }
+
+    class VolunteerFactory : IFactory
+    {
+        public Operation CreateOperation()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public LeiFeng CreateLeiFeng()
+        {
+            return  new Volunteer();
+        }
+    }
+
 }
