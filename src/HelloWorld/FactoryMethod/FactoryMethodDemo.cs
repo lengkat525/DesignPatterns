4f530e9a9b3f8ff89f674bb46965a9193f350c9d
Katrina Leng <lengkat@qq.com>
2020-07-07 16:13:44 +0800

    Factory Method demo

--- src/HelloWorld/FactoryMethod/FactoryMethodDemo.cs
+++ src/HelloWorld/FactoryMethod/FactoryMethodDemo.cs
@@ -1,9 +1,25 @@
-﻿namespace FactoryMethod
+﻿using System;
+
+namespace FactoryMethod
 {
     class FactoryMethodDemo
     {
         static void Main(string[] args)
         {
+            NewMethod();
+
+        }
+
+        private static void NewMethod()
+        {
+            IFactory operFactory = new AddFactory();
+            Operation oper = operFactory.CreateOperation();
+            oper.NumberA = 1;
+            oper.NumberB = 2;
+            double result = oper.GetResult();
+
+            Console.WriteLine(result);
+            Console.Read();
         }
     }
 }
---
+++ src/HelloWorld/FactoryMethod/IFactory.cs
@@ -0,0 +1,39 @@
+﻿namespace FactoryMethod
+{
+    interface IFactory
+    {
+        Operation CreateOperation();
+    }
+
+    class  AddFactory: IFactory
+    {
+        public Operation CreateOperation()
+        {
+            return new OperationAdd();
+        }
+    }
+
+    class SubFactory : IFactory
+    {
+        public Operation CreateOperation()
+        {
+            return new OperationSub();
+        }
+    }
+
+    class MulFactory : IFactory
+    {
+        public Operation CreateOperation()
+        {
+            return  new OperationMul();
+        }
+    }
+
+    class DivFactory : IFactory
+    {
+        public Operation CreateOperation()
+        {
+            return new OperationDiv();
+        }
+    }
+}
