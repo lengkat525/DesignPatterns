9c6790778b280256c7826196758b77f60121eb39
Katrina Leng <lengkat@qq.com>
2020-07-10 11:25:51 +0800

    Proxy

---
+++ src/Proxy/Proxy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Proxy : GiveGift
+    {
+        Pursuit _mm;
+
+        public Proxy(Girl mm)
+        {
+            _mm = new Pursuit(mm);
+        }
+
+        public void GiveDolls()
+        {
+            _mm.GiveDolls();
+        }
+
+        public void GiveFlowers()
+        {
+            _mm.GiveFlowers();
+        }
+
+        public void GiveChocolate()
+        {
+            _mm.GiveChocolate();
+        }
+
+    }
+}
---
+++ src/Proxy/Pursuit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Pursuit : GiveGift
+    {
+        readonly Girl _mm;
+
+        public Pursuit(Girl mm)
+        {
+            _mm = mm;
+        }
+
+        public void GiveDolls()
+        {
+            Console.WriteLine(_mm.Name + " Pursuit Dolls");
+        }
+
+        public void GiveFlowers()
+        {
+            Console.WriteLine(_mm.Name + " Pursuit Flowers");
+        }
+
+        public void GiveChocolate()
+        {
+            Console.WriteLine(_mm.Name + " Pursuit Chocolate");
+        }
+    }
+}
