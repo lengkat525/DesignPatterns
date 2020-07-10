9b07e8bcd464ab3cd67a77b8985650475037e55d
Katrina Leng <lengkat@qq.com>
2020-07-10 11:17:39 +0800

    PrototypeDemo

---
+++ src/Prototype/PrototypeDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Prototype
+{
+    class PrototypeDemo
+    {
+        static void Main(string[] args)
+        {
+            //NewMethod();
+            Resume leng = new Resume("Leng");
+            leng.SetPersonInfo("woman", "26");
+            leng.SetWorkExperience("2017-2020", "chr");
+            Resume leng2 = leng;
+            Resume leng3 = leng;
+
+            leng.Display();
+            leng2.Display();
+            leng3.Display();
+            Console.Read();
+        }
+
+        private static void NewMethod()
+        {
+            Resume leng = new Resume("Leng");
+            leng.SetPersonInfo("woman", "26");
+            leng.SetWorkExperience("2017-2020", "chr");
+            leng.Display();
+
+            Resume leng2 = new Resume("Leng");
+            leng2.SetPersonInfo("woman", "26");
+            leng2.SetWorkExperience("2017-2020", "chr");
+            leng2.Display();
+
+            Resume leng3 = new Resume("Leng");
+            leng3.SetPersonInfo("woman", "26");
+            leng3.SetWorkExperience("2017-2020", "chr");
+            leng3.Display();
+
+            Console.Read();
+        }
+    }
+}
---
+++ src/Prototype/Resume.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Resume
+    {
+        private string _name;
+        private string _sex;
+        private string _age;
+        private string _timeArea;
+        private string _company;
+
+        public Resume(string name)
+        {
+            _name = name;
+        }
+
+        public void SetPersonInfo(string sex, string age)
+        {
+            _sex = sex;
+            _age = age;
+        }
+
+        public void SetWorkExperience(string timeArea, string company)
+        {
+            _timeArea = timeArea;
+            _company = company;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("{0} {1} {2}", _name, _sex, _age);
+            Console.WriteLine("{0} {1}", _timeArea, _company);
+        }
+    }
+}
