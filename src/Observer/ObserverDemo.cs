a86e3a997b6a4c2f72aef7acd25c72fd59eea8b6
Katrina Leng <lengkat@qq.com>
2020-07-17 17:21:29 +0800

    abstract observer

--- src/Observer/ObserverDemo.cs
+++ src/Observer/ObserverDemo.cs
@@ -1,9 +1,39 @@
-﻿namespace Observer
+﻿using System;
+
+namespace Observer
 {
     class ObserverDemo
     {
         static void Main(string[] args)
         {
+            // NewMethod();
+            // NewMethod1();
+        }
+
+        private static void NewMethod1()
+        {
+            var secretary = new Secretary();
+            var stock1 = new StockObserver("stock1", secretary);
+            var nba = new NBAObserver("nba", secretary);
+
+            secretary.Attach(stock1);
+            secretary.Attach(nba);
+            secretary.SecretaryAction = "Close Window";
+            secretary.Notify();
+            Console.Read();
+        }
+
+        private static void NewMethod()
+        {
+            var secretary = new Secretary();
+            var stock1 = new StockObserver("stock1", secretary);
+            var stock2 = new StockObserver("stock2", secretary);
+
+            secretary.Attach(stock1);
+            secretary.Attach(stock2);
+            secretary.SecretaryAction = "Close Window";
+            secretary.Notify();
+            Console.Read();
         }
     }
 }
---
+++ src/Observer/Secretary.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Observer
+{
+    class Secretary
+    {
+        private IList<Observer> observers = new List<Observer>();
+
+        public void Attach(Observer observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Detach(Observer observer) {
+            observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (Observer o in observers)
+            {
+                o.Update();
+            }
+        }
+
+        public string SecretaryAction { get; set; }
+    }
+}
