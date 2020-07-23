a814a56c899483e1578a0611e129dad44aa310de
Katrina Leng <lengkat@qq.com>
2020-07-23 10:46:57 +0800

    translate

--- src/Adapter/Adapterdemo.cs
+++ src/Adapter/Adapterdemo.cs
@@ -1,10 +1,30 @@
-﻿namespace Adapter
+﻿using System;
+
+namespace Adapter
 {
     class AdapterDemo
     {
         static void Main(string[] args)
         {
+            //NewMethod();
+            Player a = new Forwards("A");
+            a.Attack();
+            Player b = new Guards("B");
+            b.Attack();
+            Player c = new Translator("C");
+            c.Attack();
+            c.Defense();
 
+            Console.Read();
+
+        }
+
+        private static void NewMethod()
+        {
+            Target target = new Target();
+            target.Request();
+
+            Console.Read();
         }
     }
 }
---
+++ src/Adapter/Guards.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Guards : Player
+    {
+        public Guards(string name) : base(name)
+        {
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine("Guards {0} Attack", Name);
+        }
+
+        public override void Defense()
+        {
+            Console.WriteLine("Guards {0} Defense", Name);
+        }
+    }
+}
